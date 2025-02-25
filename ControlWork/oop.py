e9d3637ee9b94b3dda54d4ab076be3c991dd7dbf
N97I <N97I-ildan3216@gmail.com>
2025-02-26 01:48:39 +0600

    Ñ2 задание готовоÑÐ

--- ControlWork/oop.py
+++ ControlWork/oop.py
@@ -1,20 +1,50 @@
-class Person:
+# class Person:
+#
+#     def __init__(self, age):
+#         self.age = age
+#
+#
+#     def set_age(self, age):
+#         if isinstance(age, int) and age > 0:
+#             self.age = age
+#         else:
+#             print(f'Ошибка: Возраст должен быть положительным')
+#
+#     def get_age(self,):
+#         return self.age
+#
+# p = Person(18)
+# p.set_age(27)
+# print(p.get_age())
+# p.set_age(-5)
 
-    def __init__(self, age):
-        self.age = age
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self,):
+        return f'I am an animal'
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self,):
+        return f'Woof'
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self,):
+        return f'Meow'
 
 
-    def set_age(self, age):
-        if isinstance(age, int) and age > 0:
-            self.age = age
-        else:
-            print(f'Ошибка: Возраст должен быть положительным')
+dog = Dog('Buddy')
+cat = Cat('Kitty')
 
-    def get_age(self,):
-        return self.age
+print(dog.name, dog.speak())
+print(cat.name, cat.speak())
 
-p = Person(18)
-p.set_age(27)
-print(p.get_age())
-p.set_age(-5)
 
